323938212d65d45789cd87edb175d22c06d1a67d
Aneta <aneta.werner@seznam.cz>
2025-07-28 17:34:17 +0200

    Folders and favicon I.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import ManualPage from '@/pages/ManualPage.vue'
+import ManualPage from '@/pages/Manuals/ManualPage.vue'
 import ToolPage from '@/pages/ToolPage.vue'
-import DictionaryPage from '@/pages/DictionaryPage.vue'
-import EducationPage from '@/pages/EducationPage.vue'
+import DictionaryPage from '@/pages/Dictionary/DictionaryPage.vue'
+import EducationPage from '@/pages/Education/EducationPage.vue'
 import HomePage from '@/pages/Homepage/HomePage.vue'
-import EshopPage from '@/pages/EshopPage.vue'
-import CardPage from '@/pages/CardPage.vue'
-import ProfilePage from '@/pages/ProfilePage.vue'
+import EshopPage from '@/pages/Eshop/EshopPage.vue'
+import CardPage from '@/pages/Card/CardPage.vue'
+import ProfilePage from '@/pages/Profile/ProfilePage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
